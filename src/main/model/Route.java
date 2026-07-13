f7fff20dbd01b3385b5ec9cf1646836d983830c2
Dominick Chen <dc0515@students.cs.ubc.ca>
2026-07-13 15:39:05 -0700

    Added class and method level specifications to Route.java and Stop.java

--- src/main/model/Route.java
+++ src/main/model/Route.java
@@ -1,15 +1,18 @@
 package model;
 
+// Represents a bus route with a direction and a name
 public class Route {
     private int routeNumber;
     private String routeName;
 
+    // REQUIRES: routeNumber is a valid TransLink bus route number
+    // EFFECTS: constructs a Route object with a route number and route name
     public Route(int routeNumber, String routeName) {
         this.routeNumber = routeNumber;
         this.routeName = routeName;
     }
 
-    // return a formatted string for a bus route
+    // EFFECTS: returns a formatted string describing a bus route
     public String makeBusRoute() {
         return routeNumber + " " + routeName;
     }
--- src/main/model/Stop.java
+++ src/main/model/Stop.java
@@ -1,11 +1,15 @@
 package model;
 
+// Represents a bus stop with a direction (NB, SB, EB, WB), name, stop ID, and timing point flag
+// A timing point is a stop where buses must wait if they arrive ahead of schedule.
 public class Stop {
     private String direction;
     private String stopName;
     private int stopID;
     private boolean isTimingPoint;
 
+    // REQUIRES: direction is one of NB, SB, EB, or WB
+    // EFFECTS: Constructs a bus stop object with a direction, stop name, stop ID, and a timing point flag
     public Stop(String direction, String stopName, int stopID, boolean isTimingPoint) {
         this.direction = direction;
         this.stopName = stopName;
@@ -13,7 +17,8 @@ public class Stop {
         this.isTimingPoint = isTimingPoint;
     }
 
-    // Return a formatted string for a bus stop
+    // REQUIRES: stopID is a 5-digit integer beginning with either 5 or 6
+    // EFFECTS: Return a formatted string describing a bus stop
     public String returnStop() {
         return direction + " " + stopName + " - Stop number: " + stopID + " - Timing point? " + isTimingPoint;
     }
