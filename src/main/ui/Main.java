91749c547f04f37e5ca558019a0c4b47f1757620
Dominick Chen <dc0515@students.cs.ubc.ca>
2026-07-14 21:29:46 -0700

    Wrote stubs and appropriate requires/modifies/effects clauses for the Main UI method

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -3,14 +3,51 @@ package ui;
 import model.Route;
 import model.Stop;
 
+// Represents the console application interface for the bus route system
 public class Main {
+    
+    // EFFECTS: runs the bus route application
     public static void main(String[] args) {
-        // Print out sample 402 route
-        Route r402 = new Route(402, "TWO ROAD");
-        System.out.println(r402.makeBusRoute());
+        new Main().runApp();
+    }
 
-        // Print out sample stop description
-        Stop s56587 = new Stop("SB", "No. 2 Rd at Steveston Hwy", 56587, false);
-        System.out.println(s56587.returnStop());
+    // EFFECTS: starts the main menu loop
+    private void runApp() {
+        // stub
+    }
+
+    // EFFECTS: prints menu options
+    private void printMenu() {
+        // stub
+    }
+
+    // REQUIRES: a user input corresponding to a valid menu option
+    // MODIFIES: this
+    // EFFECTS: processes the user command
+    private void processCommand(String command) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds a stop to the route
+    private void doAddStop() {
+        // stub
+    }
+
+    // EFFECTS: prints all stops
+    private void doListStops() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: modifies a stop name
+    private void doModifyStop() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: records a bus operator message
+    private void doRecordMessage() {
+        // stub
     }
 }
