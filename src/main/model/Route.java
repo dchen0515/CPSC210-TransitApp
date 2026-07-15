ea19805024eef0628c01cf739b46d87a45065b87
Dominick Chen <dc0515@students.cs.ubc.ca>
2026-07-14 21:10:06 -0700

    Added method bodies and getters for Route and Stop classes

--- src/main/model/Route.java
+++ src/main/model/Route.java
@@ -1,15 +1,26 @@
 package model;
 
-// Represents a bus route with a direction and a name
+import java.util.List;
+import java.util.ArrayList;
+
+
+// Represents a bus route with a direction, a name, and a list of stops on that route
 public class Route {
     private int routeNumber;
     private String routeName;
+    private List<Stop> stops;
+    private String operatorMessage;
 
-    // REQUIRES: routeNumber is a valid TransLink bus route number
-    // EFFECTS: constructs a Route object with a route number and route name
+    // REQUIRES: routeNumber is between 1 and 900 inclusive
+    // EFFECTS: constructs a Route object with a route number, route name, and list of stops
     public Route(int routeNumber, String routeName) {
+        if (routeNumber < 1 || routeNumber > 900) {
+            throw new IllegalArgumentException("Invalid route number");
+        }
+
         this.routeNumber = routeNumber;
         this.routeName = routeName;
+        stops = new ArrayList<>();
     }
 
     // EFFECTS: returns a formatted string describing a bus route
@@ -17,21 +28,56 @@ public class Route {
         return routeNumber + " " + routeName;
     }
 
+    // REQUIRES: stop s is not null
     // MODIFIES: this
     // EFFECTS: adds stop s to this route's stop list
     public void addStop(Stop s) {
-        // stub
+        if (s == null) {
+            throw new IllegalArgumentException("Stop cannot be null");
+        } else {
+            stops.add(s);
+        }
     }
 
     // EFFECTS: returns a list of formatted stops
-    public void listStops() {
-        // stub
+    public String listStops() {
+        if (stops.isEmpty()) {
+            return "";
+        } else {
+            String accumulator = "";
+
+            for (int i = 0; i < stops.size(); i++) {
+                accumulator += stops.get(i).returnStop();
+            }
+            return accumulator;
+        }
     }
 
     // REQUIRES: msg is not an empty string
     // MODIFIES: this
     // EFFECTS: simulates recording a message from the operator
     public void recordOperatorMessage(String msg) {
-        // stub
+        if (msg.isEmpty()) {
+            throw new IllegalArgumentException("Message is invalid");
+        } else {
+            operatorMessage = msg;
+        }
+    }
+
+    // Getters
+    public int getRouteNumber() {
+        return routeNumber;
+    }
+
+    public String getRouteName() {
+        return routeName;
+    }
+
+    public List<Stop> getStops() {
+        return stops;
+    }
+
+    public String getOperatorMessage() {
+        return operatorMessage;
     }
 }
--- src/main/model/Stop.java
+++ src/main/model/Stop.java
@@ -8,24 +8,55 @@ public class Stop {
     private int stopID;
     private boolean isTimingPoint;
 
-    // REQUIRES: direction is one of NB, SB, EB, or WB
+    // REQUIRES: direction is one of NB, SB, EB, or WB, and stopID is a 5-digit positive integer 
+    // beginning with either 5 or 6
     // EFFECTS: Constructs a bus stop object with a direction, stop name, stop ID, and a timing point flag
     public Stop(String direction, String stopName, int stopID, boolean isTimingPoint) {
+        if (!(direction.equals("NB")) && !(direction.equals("SB")) && !(direction.equals("EB")) && 
+        !(direction.equals("WB"))) {
+            throw new IllegalArgumentException("Direction is not valid");
+        }
+
+        if (stopID < 50000 || stopID > 69999) {
+            throw new IllegalArgumentException("Stop ID is not valid");
+        }
+
         this.direction = direction;
         this.stopName = stopName;
         this.stopID = stopID;
         this.isTimingPoint = isTimingPoint;
     }
 
-    // REQUIRES: stopID is a 5-digit positive integer beginning with either 5 or 6
     // EFFECTS: Return a formatted string describing a bus stop
     public String returnStop() {
         return direction + " " + stopName + " - Stop number: " + stopID + " - Timing point? " + isTimingPoint;
     }
 
+    // REQUIRES: newName is not an empty string
     // MODIFIES: this
     // EFFECTS: changes a stop name to reflect changes that may apply to multiple routes in the system
     public void modifyStopName(String newName) {
-         // stub
+        if (newName.isEmpty()) {
+            throw new IllegalArgumentException("New stop name is invalid");
+        } else {
+            this.stopName = newName;
+        }
+    }
+
+    // Getters
+    public String getDirection() {
+        return direction;
+    }
+
+    public String getStopName() {
+        return stopName;
+    }
+
+    public int getStopID() {
+        return stopID;
+    }
+
+    public boolean getIsTimingPoint() {
+        return isTimingPoint;
     }
 }
