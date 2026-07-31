0033d4f94a8d3b8fda0ad23d15e77e13db591f1e
Dominick Chen <dc0515@students.cs.ubc.ca>
2026-07-31 15:37:17 -0700

    Implemented StopListPanel constructor and added class+method level documentation

--- src/main/ui/StopListPanel.java
+++ src/main/ui/StopListPanel.java
@@ -1,13 +1,41 @@
 package ui;
 
 import javax.swing.*;
+
+import ca.ubc.cs.ExcludeFromJacocoGeneratedReport;
+
 import java.awt.*;
 
-public class StopListPanel extends JPanel{
+import java.util.List;
+import java.util.ArrayList;
+
+import model.Stop;
+
+/*
+ * Represents the panel in the TransitApp GUI that displays the list of stops
+ * currently associated with the route.
+ */
+@ExcludeFromJacocoGeneratedReport
+public class StopListPanel extends JPanel {
     private DefaultListModel<String> model;
     private JList<String> list;
 
+    // MODIFIES: this
+    // EFFECTS: initializes the StopList panel with a scrollable JList
     public StopListPanel() {
-        // constructor stub
+        setLayout(new BorderLayout());
+
+        model = new DefaultListModel<>();
+        list = new JList<>(model);
+
+        JScrollPane scrollPane = new JScrollPane(list);
+        add(scrollPane, BorderLayout.CENTER);
+    }
+
+    // REQUIRES: stops is not null
+    // MODIFIES: this
+    // EFFECTS: clears the current list and displays the given stops in the panel
+    public void showStops(List<Stop> stops) {
+        // stub
     }
 }
