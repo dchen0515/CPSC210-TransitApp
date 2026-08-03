2b3e9606b4725d3823b39eb689092b00a1cd3a5b
Dominick Chen <dc0515@students.cs.ubc.ca>
2026-08-03 15:43:25 -0700

    Implemented OperatorMessagePanel constructor and refresh method

--- src/main/ui/OperatorMessagePanel.java
+++ src/main/ui/OperatorMessagePanel.java
@@ -11,13 +11,29 @@ public class OperatorMessagePanel extends JPanel {
     private JList<String> list;
 
     public OperatorMessagePanel() {
-        // stub
+        setLayout(new BorderLayout());
+
+        model = new DefaultListModel<>();
+        list = new JList<>(model);
+
+        list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        list.setLayoutOrientation(JList.VERTICAL);
+        list.setVisibleRowCount(-1);
+
+        JScrollPane scrollPane = new JScrollPane(list);
+        add(new JLabel("Operator Messages"), BorderLayout.NORTH);
+        add(scrollPane, BorderLayout.CENTER);
     }
 
     // MODIFIES: this
     // EFFECTS: rebuilds the list of operator messages from all routes
     public void refresh(RouteManager manager) {
-        // stub
+        model.clear();
+
+        for (Route route : manager.getRoutes()) {
+            for (String msg : route.getOperatorMessages()) {
+                model.addElement(route.getRouteNumber() + ": " + msg);
+            }
+        }
     }
 }
-
