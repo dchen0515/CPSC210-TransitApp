a6352a7d9bca7f0557d2adbd3ac22493edd5fa76
Dominick Chen <dc0515@students.cs.ubc.ca>
2026-08-03 11:59:43 -0700

    Added documentation for makeImageLabel method in VisualPanel class

--- src/main/ui/VisualPanel.java
+++ src/main/ui/VisualPanel.java
@@ -19,7 +19,7 @@ public class VisualPanel extends JPanel {
         setPreferredSize(new Dimension(600, 450));
 
         JPanel grid = new JPanel(new GridLayout(2, 3, 5, 5));
-        
+
         grid.add(makeImageLabel("9285.jpg"));
         grid.add(makeImageLabel("8115.jpg"));
         grid.add(makeImageLabel("21414.jpg"));
@@ -31,10 +31,12 @@ public class VisualPanel extends JPanel {
         add(grid, BorderLayout.CENTER);
     }
 
+    // REQUIRES: filename is the name of an image file in the folder src/images/
+    // EFFECTS: loads image with given filename from src/images/, scales it down
+    // to 250x250, and returns a JLabel with the scaled image centered horizontally
     private JLabel makeImageLabel(String filename) {
         ImageIcon icon = new ImageIcon("src/images/" + filename);
         Image scaled = icon.getImage().getScaledInstance(250, 250, Image.SCALE_SMOOTH);
         return new JLabel(new ImageIcon(scaled), SwingConstants.CENTER);
     }
-
 }
